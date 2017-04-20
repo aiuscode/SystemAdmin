80a9c9b746004aadb3c2d5d0f9f80d4f7d03c3fd
anascimento <anascimento@anascimento-ti>
2017-04-20 00:04:05 -0300

    Projeto para gerenciamento de usuários desligados e afastados

---
+++ pages-gallery.php
@@ -0,0 +1,251 @@
+<?php require 'includes/header_start.php'; ?>
+<!-- extra css -->
+<!--venobox lightbox-->
+<link rel="stylesheet" href="assets/plugins/magnific-popup/css/magnific-popup.css"/>
+
+<?php require 'includes/header_end.php'; ?>
+
+
+<!-- ============================================================== -->
+<!-- Start right Content here -->
+<!-- ============================================================== -->
+<div class="content-page">
+    <!-- Start content -->
+    <div class="content">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="page-title-box">
+                        <h4 class="page-title">Gallery</h4>
+                        <ol class="breadcrumb p-0">
+                            <li>
+                                <a href="#">Uplon</a>
+                            </li>
+                            <li>
+                                <a href="#">Pages</a>
+                            </li>
+                            <li class="active">
+                                Gallery
+                            </li>
+                        </ol>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+            <!-- end row -->
+
+
+            <!-- SECTION FILTER
+            ================================================== -->
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 ">
+                    <div class="portfolioFilter">
+                        <a href="#" data-filter="*" class="current waves-effect waves-light">All</a>
+                        <a href="#" data-filter=".natural" class="waves-effect waves-light">Natural</a>
+                        <a href="#" data-filter=".creative" class="waves-effect waves-light">Creative</a>
+                        <a href="#" data-filter=".personal" class="waves-effect waves-light">Personal</a>
+                        <a href="#" data-filter=".photography" class="waves-effect waves-light">Photography</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row port m-b-20">
+                <div class="portfolioContainer">
+                    <div class="col-sm-6 col-md-4 natural personal">
+                        <div class="thumb">
+                            <a href="assets/images/gallery/12.jpg" class="image-popup" title="Screenshot-1">
+                                <img src="assets/images/gallery/12.jpg" class="thumb-img" alt="work-thumbnail">
+                            </a>
+                            <div class="gal-detail text-xs-center">
+                                <h4 class="m-t-10">Nature Image</h4>
+                                <p class="text-muted">
+                                    Web Design , Graphic Design
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 creative personal photography">
+                        <div class="thumb">
+                            <a href="assets/images/gallery/2.jpg" class="image-popup" title="Screenshot-2">
+                                <img src="assets/images/gallery/2.jpg" class="thumb-img" alt="work-thumbnail">
+                            </a>
+                            <div class="gal-detail text-xs-center">
+                                <h4 class="m-t-10">Nature Image</h4>
+                                <p class="text-muted">
+                                    Web Design , Graphic Design
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 natural creative">
+                        <div class=" thumb">
+                            <a href="assets/images/gallery/3.jpg" class="image-popup" title="Screenshot-3">
+                                <img src="assets/images/gallery/3.jpg" class="thumb-img" alt="work-thumbnail">
+                            </a>
+                            <div class="gal-detail text-xs-center">
+                                <h4 class="m-t-10">Nature Image</h4>
+                                <p class="text-muted">
+                                    Web Design , Graphic Design
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 personal photography">
+                        <div class="thumb">
+                            <a href="assets/images/gallery/4.jpg" class="image-popup" title="Screenshot-4">
+                                <img src="assets/images/gallery/4.jpg" class="thumb-img" alt="work-thumbnail">
+                            </a>
+                            <div class="gal-detail text-xs-center">
+                                <h4 class="m-t-10">Nature Image</h4>
+                                <p class="text-muted">
+                                    Web Design , Graphic Design
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 creative photography">
+                        <div class="thumb">
+                            <a href="assets/images/gallery/9.jpg" class="image-popup" title="Screenshot-5">
+                                <img src="assets/images/gallery/9.jpg" class="thumb-img" alt="work-thumbnail">
+                            </a>
+                            <div class="gal-detail text-xs-center">
+                                <h4 class="m-t-10">Nature Image</h4>
+                                <p class="text-muted">
+                                    Web Design , Graphic Design
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 natural photography">
+                        <div class="thumb">
+                            <a href="assets/images/gallery/6.jpg" class="image-popup" title="Screenshot-6">
+                                <img src="assets/images/gallery/6.jpg" class="thumb-img" alt="work-thumbnail">
+                            </a>
+                            <div class="gal-detail text-xs-center">
+                                <h4 class="m-t-10">Nature Image</h4>
+                                <p class="text-muted">
+                                    Web Design , Graphic Design
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 personal photography creative">
+                        <div class="thumb">
+                            <a href="assets/images/gallery/7.jpg" class="image-popup" title="Screenshot-7">
+                                <img src="assets/images/gallery/7.jpg" class="thumb-img" alt="work-thumbnail">
+                            </a>
+                            <div class="gal-detail text-xs-center">
+                                <h4 class="m-t-10">Nature Image</h4>
+                                <p class="text-muted">
+                                    Web Design , Graphic Design
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 creative photography natural">
+                        <div class="thumb">
+                            <a href="assets/images/gallery/8.jpg" class="image-popup" title="Screenshot-8">
+                                <img src="assets/images/gallery/8.jpg" class="thumb-img" alt="work-thumbnail">
+                            </a>
+                            <div class="gal-detail text-xs-center">
+                                <h4 class="m-t-10">Nature Image</h4>
+                                <p class="text-muted">
+                                    Web Design , Graphic Design
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 natural personal">
+                        <div class="thumb">
+                            <a href="assets/images/gallery/5.jpg" class="image-popup" title="Screenshot-9">
+                                <img src="assets/images/gallery/5.jpg" class="thumb-img" alt="work-thumbnail">
+                            </a>
+                            <div class="gal-detail text-xs-center">
+                                <h4 class="m-t-10">Nature Image</h4>
+                                <p class="text-muted">
+                                    Web Design , Graphic Design
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 photography creative">
+                        <div class="thumb">
+                            <a href="assets/images/gallery/10.jpg" class="image-popup" title="Screenshot-10">
+                                <img src="assets/images/gallery/10.jpg" class="thumb-img" alt="work-thumbnail">
+                            </a>
+                            <div class="gal-detail text-xs-center">
+                                <h4 class="m-t-10">Nature Image</h4>
+                                <p class="text-muted">
+                                    Web Design , Graphic Design
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 creative photography">
+                        <div class="thumb">
+                            <a href="assets/images/gallery/11.jpg" class="image-popup" title="Screenshot-11">
+                                <img src="assets/images/gallery/11.jpg" class="thumb-img" alt="work-thumbnail">
+                            </a>
+                            <div class="gal-detail text-xs-center">
+                                <h4 class="m-t-10">Nature Image</h4>
+                                <p class="text-muted">
+                                    Web Design , Graphic Design
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-4 natural creative personal">
+                        <div class="thumb">
+                            <a href="assets/images/gallery/1.jpg" class="image-popup" title="Screenshot-12">
+                                <img src="assets/images/gallery/1.jpg" class="thumb-img" alt="work-thumbnail">
+                            </a>
+                            <div class="gal-detail text-xs-center">
+                                <h4 class="m-t-10">Nature Image</h4>
+                                <p class="text-muted">
+                                    Web Design , Graphic Design
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div><!-- end portfoliocontainer-->
+            </div> <!-- End row -->
+
+
+
+        </div> <!-- container -->
+
+    </div> <!-- content -->
+
+</div>
+<!-- End content-page -->
+
+
+<!-- ============================================================== -->
+<!-- End Right content here -->
+<!-- ============================================================== -->
+
+<?php require 'includes/footer_start.php' ?>
+<!-- extra js -->
+<!-- isotope filter plugin -->
+<script type="text/javascript" src="assets/plugins/isotope/js/isotope.pkgd.min.js"></script>
+
+<!-- Magnific popup -->
+<script type="text/javascript" src="assets/plugins/magnific-popup/js/jquery.magnific-popup.min.js"></script>
+
+<script type="text/javascript" src="assets/pages/jquery.gallery.init.js"></script>
+
+
+<?php require 'includes/footer_end.php' ?>
---
+++ pages-pricing.php
@@ -0,0 +1,125 @@
+<?php require 'includes/header_start.php'; ?>
+<!-- extra css -->
+<?php require 'includes/header_end.php'; ?>
+
+
+<!-- ============================================================== -->
+<!-- Start right Content here -->
+<!-- ============================================================== -->
+<div class="content-page">
+    <!-- Start content -->
+    <div class="content">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="page-title-box">
+                        <h4 class="page-title">Pricing</h4>
+                        <ol class="breadcrumb p-0">
+                            <li>
+                                <a href="#">Uplon</a>
+                            </li>
+                            <li>
+                                <a href="#">Pages</a>
+                            </li>
+                            <li class="active">
+                                Pricing
+                            </li>
+                        </ol>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+            <!-- end row -->
+
+            <div class="row">
+                <div class="col-xl-10 col-xl-offset-1">
+
+                    <div class="text-xs-center">
+                        <h3 class="m-b-30 m-t-20">Choose your perfect plan</h3>
+                        <p class="text-muted">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has <br> been the industry's standard dummy text ever since the
+                            1500s.
+                        </p>
+                    </div>
+
+                    <div class="m-t-30 m-b-30">
+                        <div class="row">
+                            <div class="col-sm-12 col-xs-12 col-md-4 col-lg-4">
+                                <div class="price_card text-xs-center">
+                                    <div class="pricing-header bg-primary">
+                                        <span class="price">$19</span>
+                                        <span class="name">Starter Pack</span>
+                                    </div>
+                                    <ul class="price-features">
+                                        <li>10 GB Storage</li>
+                                        <li>500 GB Bandwidth</li>
+                                        <li>No Domain</li>
+                                        <li>1 User</li>
+                                        <li>Email Support</li>
+                                        <li>24x7 Support</li>
+                                    </ul>
+                                    <button class="btn btn-primary waves-effect waves-light w-md">Sign Up</button>
+                                </div> <!-- end Pricing_card -->
+                            </div> <!-- end col -->
+
+                            <div class="col-sm-12 col-xs-12 col-md-4 col-lg-4">
+                                <div class="price_card active text-xs-center">
+                                    <div class="pricing-header bg-pink">
+                                        <span class="price">$29</span>
+                                        <span class="name">Professional Pack</span>
+                                    </div>
+                                    <ul class="price-features">
+                                        <li>50 GB Storage</li>
+                                        <li>900 GB Bandwidth</li>
+                                        <li>2 Domain</li>
+                                        <li>10 User</li>
+                                        <li>Email Support</li>
+                                        <li>24x7 Support</li>
+                                    </ul>
+                                    <button class="btn btn-pink w-md waves-effect waves-light">Sign Up</button>
+                                </div>
+                            </div> <!-- end col -->
+
+                            <div class="col-sm-12 col-xs-12 col-md-4 col-lg-4">
+                                <div class="price_card text-xs-center">
+                                    <div class="pricing-header bg-purple">
+                                        <span class="price">$39</span>
+                                        <span class="name">Enterprise Pack</span>
+                                    </div>
+                                    <ul class="price-features">
+                                        <li>100 GB Storege</li>
+                                        <li>Unlimited Bandwidth</li>
+                                        <li>10 Domain</li>
+                                        <li>Unlimited User</li>
+                                        <li>Email Support</li>
+                                        <li>24x7 Support</li>
+                                    </ul>
+                                    <button class="btn btn-purple w-md waves-effect waves-light">Sign Up</button>
+                                </div> <!-- end Pricing_card -->
+                            </div> <!-- end col -->
+
+                        </div>
+                        <!-- end row -->
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div> <!-- container -->
+
+    </div> <!-- content -->
+
+</div>
+<!-- End content-page -->
+
+
+<!-- ============================================================== -->
+<!-- End Right content here -->
+<!-- ============================================================== -->
+
+<?php require 'includes/footer_start.php' ?>
+<!-- extra js -->
+<?php require 'includes/footer_end.php' ?>
